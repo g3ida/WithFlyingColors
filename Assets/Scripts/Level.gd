177f4286a02315f4b2db200ab90e85053c2c97f7
g3ida <med.gaida@gmail.com>
2022-08-10 21:55:39 +0100

    reset scene when player dies

---
+++ Assets/Scripts/Level.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+export var level_path: String
+
+func connect_signals():
+	Event.connect("player_died", self, "_on_game_over")
+
+func disconnect_signals():
+	Event.disconnect("player_died", self, "_on_game_over")
+	
+func _enter_tree():
+	connect_signals()
+
+func _exit_tree():
+	disconnect_signals()
+	
+func _on_game_over():
+	get_tree().change_scene(self.level_path)
