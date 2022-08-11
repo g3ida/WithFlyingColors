cdeaa81543ea3e41560c390c345aaa3891032099
g3ida <med.gaida@gmail.com>
2022-08-11 09:50:43 +0100

    added fallzone

---
+++ Assets/Scripts/Fallzone.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_FallzoneArea_area_entered(area):
+	Event.emit_signal("player_died")
