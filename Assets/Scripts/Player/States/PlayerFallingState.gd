b28009e232bab392bb067cd646d2449385f7f2af
System Administrator <root@MacBook-Pro-de-a.local>
2022-08-16 19:02:44 +0100

    add responsiveness to jump action

--- Assets/Scripts/Player/States/PlayerFallingState.gd
+++ Assets/Scripts/Player/States/PlayerFallingState.gd
@@ -1,13 +1,18 @@
 class_name PlayerFallingState
 extends PlayerBaseState
 
+const CountdownTimer = preload("res://Assets/Scripts/Utils/CountdownTimer.gd")
+var permissiveness_timer: CountdownTimer
+const PERMISSIVENESS = 0.04
+
 func _init(dependencies: PlayerDependencies).(dependencies):
-	pass
+	permissiveness_timer = CountdownTimer.new(PERMISSIVENESS, false)
 func enter():
 	animated_sprite.play("idle")
 	animated_sprite.playing = false
+	permissiveness_timer.reset()
 func exit():
-	pass
+	permissiveness_timer.stop()
 
 func physics_update(delta: float) -> BaseState:
 	return .physics_update(delta)
@@ -15,6 +20,10 @@ func physics_update(delta: float) -> BaseState:
 func _physics_update(_delta: float) -> BaseState:
 	if (player.is_on_floor()):
 		return self.states_store.get_state(PlayerStatesEnum.STANDING)
+	if Input.is_action_just_pressed("jump") and permissiveness_timer.is_running():
+		permissiveness_timer.stop()
+		return self.states_store.get_state(PlayerStatesEnum.JUMPING)
+	permissiveness_timer.step(_delta)
 	return null
 func on_animation_finished(_anim_name) -> BaseState:
 	return null
--- Assets/Scripts/Player/States/PlayerJumpingState.gd
+++ Assets/Scripts/Player/States/PlayerJumpingState.gd
@@ -3,19 +3,25 @@ extends PlayerBaseState
 
 const CountdownTimer = preload("res://Assets/Scripts/Utils/CountdownTimer.gd")
 const TIME_UNTIL_FULL_JUMP_IS_CONSIDERED = 0.15
+const PERMISSIVENESS = 0.09
 
 const JUMP_FORCE = 1000
 var entred = false
 var jump_timer: CountdownTimer
+var permissiveness_timer: CountdownTimer
 
 func _init(dependencies: PlayerDependencies).(dependencies):
 	jump_timer = CountdownTimer.new(TIME_UNTIL_FULL_JUMP_IS_CONSIDERED, false)
+	permissiveness_timer = CountdownTimer.new(PERMISSIVENESS, false)
+
 func enter():
 	entred = true
 	jump_timer.reset()
+
 func exit():
 	entred = false
 	jump_timer.stop()
+	permissiveness_timer.stop()
 
 func physics_update(delta: float) -> BaseState:
 	return .physics_update(delta)
@@ -25,13 +31,20 @@ func _physics_update(_delta: float) -> BaseState:
 		entred = false
 		player.velocity.y -= JUMP_FORCE
 	elif (player.is_on_floor()):
-		return self.states_store.get_state(PlayerStatesEnum.STANDING)
-	
+		if permissiveness_timer.is_running():
+			return self.states_store.get_state(PlayerStatesEnum.JUMPING)
+		else:
+			return self.states_store.get_state(PlayerStatesEnum.STANDING)
+
+	if (Input.is_action_just_pressed("jump")):
+		permissiveness_timer.reset()
+
 	if jump_timer.is_running() and Input.is_action_just_released("jump"):
 		jump_timer.stop()
 		if (player.velocity.y < 0):
 			player.velocity.y *= 0.5
 
 	jump_timer.step(_delta)
+	permissiveness_timer.step(_delta)
 
 	return null
