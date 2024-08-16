5345468872d9848251be9b6484acce4914273724
g3ida <med.gaida@gmail.com>
2024-08-16 19:39:25 +0200

    [Refactor] Refactor main menu

--- src/Scripts/Autoload/MenuManager.cs
+++ src/Scripts/Autoload/MenuManager.cs
@@ -1,99 +1,101 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class MenuManager : Node2D
 {
+  private static MenuManager _instance = null;
 
-    private static MenuManager _instance = null;
+  public override void _Ready()
+  {
+    base._Ready();
+    _instance = GetTree().Root.GetNode<MenuManager>("MenuManagerCS");
+    SetProcess(false);
+  }
 
-    public override void _Ready() {
-      base._Ready();
-      _instance = GetTree().Root.GetNode<MenuManager>("MenuManagerCS");
-      SetProcess(false);
-    }
+  public static MenuManager Instance()
+  {
+    return _instance;
+  }
 
-    public static MenuManager Instance() {
-      return _instance;
-    }
+  public const string SETTINGS_MENU_SCENE = "res://Assets/Screens/SettingsMenu.tscn";
+  public const string STATS_MENU_SCENE = "res://Assets/Screens/StatsMenu.tscn";
+  public const string MAIN_MENU_SCENE = "res://src/Wfc/Entities/Ui/Menu/MainMenu.tscn";
+  public const string SELECT_SLOT_SCENE = "res://Assets/Screens/SelectSlotMenu.tscn";
+  public const string LEVEL_SELECT_SCENE = "res://Assets/Screens/LevelSelectMenu.tscn";
 
-    public const string SETTINGS_MENU_SCENE = "res://Assets/Screens/SettingsMenu.tscn";
-    public const string STATS_MENU_SCENE = "res://Assets/Screens/StatsMenu.tscn";
-    public const string MAIN_MENU_SCENE = "res://Assets/Screens/MainMenu.tscn";
-    public const string SELECT_SLOT_SCENE = "res://Assets/Screens/SelectSlotMenu.tscn";
-    public const string LEVEL_SELECT_SCENE = "res://Assets/Screens/LevelSelectMenu.tscn";
-    //public const string START_LEVEL_MENU_SCENE = "res://Levels/Level1.tscn";
-     public const string START_LEVEL_MENU_SCENE = "res://Levels/TutorialLevel.tscn";
-    private const string SCENE_ORCHESTER_SCENE = "res://Assets/Scenes/SceneOrchester.tscn";
-    private const string LEVEL_CLEAR_SCENE = "res://Assets/Screens/LevelClearedMenu.tscn";
+  //public const string START_LEVEL_MENU_SCENE = "res://Levels/Level1.tscn";
+  public const string START_LEVEL_MENU_SCENE = "res://Levels/TutorialLevel.tscn";
+  private const string SCENE_ORCHESTER_SCENE = "res://Assets/Scenes/SceneOrchester.tscn";
+  private const string LEVEL_CLEAR_SCENE = "res://Assets/Screens/LevelClearedMenu.tscn";
 
-    public enum Menus
+  public enum Menus
+  {
+    SETTINGS_MENU,
+    SELECT_SLOT,
+    STATS_MENU,
+    MAIN_MENU,
+    LEVEL_SELECT_MENU,
+    LEVEL_CLEAR_MENU,
+    GAME,
+    QUIT,
+    LOAD
+  }
+
+  private Menus currentMenu = Menus.MAIN_MENU;
+  private Menus previousMenu = Menus.GAME;
+
+  public Menus PreviousMenu
+  {
+    get { return previousMenu; }
+  }
+
+  public string levelScenePath = "";
+
+  private string GetMenuScenePath(Menus menu)
+  {
+    if (menu == Menus.GAME)
     {
-        SETTINGS_MENU,
-        SELECT_SLOT,
-        STATS_MENU,
-        MAIN_MENU,
-        LEVEL_SELECT_MENU,
-        LEVEL_CLEAR_MENU,
-        GAME,
-        QUIT,
-        LOAD
+      return SCENE_ORCHESTER_SCENE;
     }
-
-    private Menus currentMenu = Menus.MAIN_MENU;
-    private Menus previousMenu = Menus.GAME;
-
-    public Menus PreviousMenu
+    else
     {
-        get { return previousMenu; }
+      levelScenePath = "";
+      switch (menu)
+      {
+        case Menus.SETTINGS_MENU:
+          return SETTINGS_MENU_SCENE;
+        case Menus.STATS_MENU:
+          return STATS_MENU_SCENE;
+        case Menus.MAIN_MENU:
+          return MAIN_MENU_SCENE;
+        case Menus.LEVEL_CLEAR_MENU:
+          return LEVEL_CLEAR_SCENE;
+        case Menus.SELECT_SLOT:
+          return SELECT_SLOT_SCENE;
+        case Menus.LEVEL_SELECT_MENU:
+          return LEVEL_SELECT_SCENE;
+        default:
+          return null;
+      }
     }
+  }
 
-    public string levelScenePath = "";
-
-    private string GetMenuScenePath(Menus menu)
+  public void GoToMenu(Menus nextMenu)
+  {
+    if (currentMenu != nextMenu)
     {
-        if (menu == Menus.GAME)
-        {
-            return SCENE_ORCHESTER_SCENE;
-        }
-        else
-        {
-            levelScenePath = "";
-            switch (menu)
-            {
-                case Menus.SETTINGS_MENU:
-                    return SETTINGS_MENU_SCENE;
-                case Menus.STATS_MENU:
-                    return STATS_MENU_SCENE;
-                case Menus.MAIN_MENU:
-                    return MAIN_MENU_SCENE;
-                case Menus.LEVEL_CLEAR_MENU:
-                    return LEVEL_CLEAR_SCENE;
-                case Menus.SELECT_SLOT:
-                    return SELECT_SLOT_SCENE;
-                case Menus.LEVEL_SELECT_MENU:
-                    return LEVEL_SELECT_SCENE;
-                default:
-                    return null;
-            }
-        }
+      previousMenu = currentMenu;
+      currentMenu = nextMenu;
+      string scenePath = GetMenuScenePath(nextMenu);
+      if (scenePath != null)
+      {
+        GetTree().ChangeSceneToFile(scenePath);
+      }
     }
+  }
 
-    public void GoToMenu(Menus nextMenu)
-    {
-        if (currentMenu != nextMenu)
-        {
-            previousMenu = currentMenu;
-            currentMenu = nextMenu;
-            string scenePath = GetMenuScenePath(nextMenu);
-            if (scenePath != null)
-            {
-                GetTree().ChangeSceneToFile(scenePath);
-            }
-        }
-    }
-
-    public void SetCurrentLevel(string _levelScenePath)
-    {
-        levelScenePath = _levelScenePath;
-    }
+  public void SetCurrentLevel(string _levelScenePath)
+  {
+    levelScenePath = _levelScenePath;
+  }
 }
--- src/Scripts/Menus/LevelClearedMenu/LevelClearedMenu.cs
+++ src/Scripts/Menus/LevelClearedMenu/LevelClearedMenu.cs
@@ -2,28 +2,22 @@ using Godot;
 using System;
 using System.Xml.Serialization;
 
-public partial class LevelClearedMenu : GameMenu
-{
-  public override void _Ready()
-  {
+public partial class LevelClearedMenu : GameMenu {
+  public override void _Ready() {
     base._Ready();
   }
 
-  public override void _Input(InputEvent ev)
-  {
+  public override void _Input(InputEvent ev) {
     base._Input(ev);
     // Handle input based on input type
-    if (ev is InputEventKey)
-    {
+    if (ev is InputEventKey) {
       Event.Instance.EmitMenuButtonPressed(MenuButtons.EXIT_LEVEL_CLEAR);
     }
   }
 
-  public override bool on_menu_button_pressed(MenuButtons menuButton)
-  {
-    base.on_menu_button_pressed(menuButton);
-    if (menuButton == MenuButtons.EXIT_LEVEL_CLEAR)
-    {
+  public override bool OnMenuButtonPressed(MenuButtons menuButton) {
+    base.OnMenuButtonPressed(menuButton);
+    if (menuButton == MenuButtons.EXIT_LEVEL_CLEAR) {
       NavigateToScreen(MenuManager.Menus.MAIN_MENU);
       return true;
     }
