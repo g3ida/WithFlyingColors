bed60524802728f124e41ffc0705137b943970fd
g3ida <med.gaida@gmail.com>
2024-08-24 16:44:52 +0200

    [DI] Add logger

--- src/Wfc/Base/DependenciesProvider.cs
+++ src/Wfc/Base/DependenciesProvider.cs
@@ -6,15 +6,20 @@ using Godot;
 using Wfc.Core.Localization;
 using Wfc.Screens.MenuManager;
 using System;
+using Wfc.Core.Logger;
 
 [Meta(typeof(IAutoNode))]
-public partial class DependenciesProvider : Node, IProvide<IMenuManager>, IProvide<ILocalizationService> {
+public partial class DependenciesProvider : Node,
+  IProvide<ILogger>, IProvide<IMenuManager>, IProvide<ILocalizationService> {
   public override void _Notification(int what) => this.Notify(what);
 
   private readonly Lazy<IMenuManager> _menuManager;
+  private readonly ILogger _logger = new GDLogger();
 
   IMenuManager IProvide<IMenuManager>.Value() => _menuManager.Value;
   ILocalizationService IProvide<ILocalizationService>.Value() => new LocalizationService();
+  ILogger IProvide<ILogger>.Value() => _logger;
+
 
   public DependenciesProvider() : base() {
     _menuManager = new Lazy<IMenuManager>(() => new MenuManager(this));
