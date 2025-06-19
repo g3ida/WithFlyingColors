bb07e885c908bcb8b16182edd231c5f3bcbac1b5
g3ida <med.gaida@gmail.com>
2025-06-19 18:27:30 +0200

    Fix miscellaneous issues in TutorialLevel

--- src/Scripts/CutScene/CutsceneTrigger.cs
+++ src/Scripts/CutScene/CutsceneTrigger.cs
@@ -1,39 +1,32 @@
-using Godot;
 using System.Collections.Generic;
+using Godot;
 
-public partial class CutsceneTrigger : Area2D
-{
-    private Marker2D followChild = null;
-    private bool triggered = false;
+public partial class CutsceneTrigger : Area2D {
+  private Marker2D followChild = null;
+  private bool triggered = false;
 
-    public override void _Ready()
-    {
-        var children = GetChildren();
-        foreach (var ch in children)
-        {
-            if (ch is Marker2D position2D)
-            {
-                followChild = position2D;
-            }
-        }
+  public override void _Ready() {
+    var children = GetChildren();
+    foreach (var ch in children) {
+      if (ch is Marker2D position2D) {
+        followChild = position2D;
+      }
     }
+  }
 
-    private void _on_Area2D_body_entered(Node body)
-    {
-        if (!triggered && body == Global.Instance().Player && followChild != null)
-        {
-            triggered = true;
-            Global.Instance().Cutscene.ShowSomeNode(followChild, 3.0f, 3.2f);
-        }
+  private void _onBodyEntered(Node body) {
+    if (!triggered && body == Global.Instance().Player && followChild != null) {
+      triggered = true;
+      Global.Instance().Cutscene.ShowSomeNode(followChild, 3.0f, 3.2f);
     }
+  }
 
-    public override void _EnterTree()
-    {
-        // Connect("body_entered", this, nameof(_on_Area2D_body_entered));
-    }
+  public override void _EnterTree() {
+    Connect("body_entered", new Callable(this, nameof(_onBodyEntered)), (uint)ConnectFlags.Persist);
 
-    public override void _ExitTree()
-    {
-        // Disconnect("body_entered", this, nameof(_on_Area2D_body_entered));
-    }
+  }
+
+  public override void _ExitTree() {
+    Disconnect("body_entered", new Callable(this, nameof(_onBodyEntered)));
+  }
 }
